eca77d932185e2872526404e44cec9ab5952c760
erik <erik.cr42@gmail.com>
2024-11-20 16:42:18 -0300

    Implementing deviceController functions

---
+++ src/controllers/deviceController.js
@@ -0,0 +1,30 @@
+
+// Write a REST service that supports the management of a device database.
+// Represented entities
+
+// 1. Device
+//     o Device name
+//     o Device brand
+//     o Creation time
+const devices = [];
+
+// Supported operations
+
+// 1. Add device;
+exports.addDevice = (req, res) => {
+};
+// 2. Get device by identifier;
+exports.getDeviceById = (req, res) => {
+};
+// 3. List all devices;
+exports.listAllDevices = (req, res) => {
+};
+// 4. Update device (full and partial);
+exports.updateDevice = (req, res) => {
+};
+// 5. Delete a device;
+exports.deleteDevice = (req, res) => {
+};
+// 6. Search device by brand;
+exports.searchDeviceByBrand = (req, res) => {
+};
