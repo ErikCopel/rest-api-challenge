351517b65a556b75ed5a665511f091863ea3668c
erik <erik.cr42@gmail.com>
2024-11-20 17:24:06 -0300

    Implements addDevice and listAllDevices

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -7,17 +7,32 @@
 //     o Device brand
 //     o Creation time
 const devices = [];
+let idCount = 1;
 
 // Supported operations
 
 // 1. Add device;
 exports.addDevice = (req, res) => {
+    console.log(req.body);
+    const { name, brand } = req.body;
+    if (!name || !brand) {
+        return res.status(400).json({ error: 'name and brand are required' });
+    }
+    const newDevice = {
+        id: idCount++,
+        name,
+        brand,
+        creationTime: new Date()
+    };
+    devices.push(newDevice);
+    res.status(201).json(newDevice);
 };
 // 2. Get device by identifier;
 exports.getDeviceById = (req, res) => {
 };
 // 3. List all devices;
 exports.listAllDevices = (req, res) => {
+    res.json(devices);
 };
 // 4. Update device (full and partial);
 exports.updateDevice = (req, res) => {
