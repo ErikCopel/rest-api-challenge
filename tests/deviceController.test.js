9581dcbfbbb182a8e5135a556efd5b1042067ce5
erik <erik.cr42@gmail.com>
2024-11-21 10:31:46 -0300

    Defines API tests

---
+++ tests/deviceController.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe("Device API", () => {
+    it("should add a device", async () => {
+    });
+
+    it("should list all devices", async () => {
+    });
+
+    it("should get a device by id", async () => {
+    })
+
+    it("should update a device", async () => {
+    })
+
+    it("should delete a device", async () => {
+    })
+
+    it("should search a device by brand", async () => {
+    })
+});
